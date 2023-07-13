a02eacd54261476b6253740d61266f321d33bdcc
Kishor04 <kishorditiss@gmail.com>
2023-07-13 23:07:23 +0530

    first python code

---
+++ big.py
@@ -0,0 +1,9 @@
+#/usr/bin/python3
+a = int(input("Enter first Number : "))
+b = int(input("Enter second Number : "))
+if a < b :
+	print("Number",a," is less than",b)
+elif a > b :
+	print("Number",a," is greater than",b)
+else:
+	print("Number",a," is equals to",b)
